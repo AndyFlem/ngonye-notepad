aa921cac1a87dd55fcad86321a0d0f1d2e44750d
Andy Fleming <andy@ulendo.com>
2020-03-10 17:26:46 +0200

    update

--- h_cc_runner_script.py
+++ h_cc_runner_script.py
@@ -11,7 +11,7 @@ cc_case='WC2'
 sims=pd.read_csv(output_data + 'CC/climate_change_sims_' + cc_case + '.csv').set_index('SimNo')
 
 for sim in sims.itertuples():
-    if sim.Index>0:
-        pm.execute_notebook('d_ewr_setup.ipynb','-',parameters = dict(input_file='CC/ngonye_daily_' + str(cc_case) + '_' + str(sim.Index)))
-        pm.execute_notebook('e_power_energy.ipynb','-',parameters = dict(input_file='CC/ngonye_daily_' + str(cc_case) + '_' + str(sim.Index)))
+    if sim.Index>=0:
+        #pm.execute_notebook('d_ewr_setup.ipynb','-',parameters = dict(input_file='CC/ngonye_daily_' + str(cc_case) + '_' + str(sim.Index)))
+        #pm.execute_notebook('e_power_energy.ipynb','-',parameters = dict(input_file='CC/ngonye_daily_' + str(cc_case) + '_' + str(sim.Index)))
         
