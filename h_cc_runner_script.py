8e97364c36be08a77559fdf98778e0ad1a5feaae
Andy Fleminf <andy@ulendo.com>
2020-03-11 15:00:30 +0200

    update

--- h_cc_runner_script.py
+++ h_cc_runner_script.py
@@ -6,13 +6,13 @@ import datetime
 
 input_data='./input_data/'
 output_data='./output_data/'
-cc_case='WC1'
+cc_case='WC2'
 
 sims=pd.read_csv(output_data + 'CC/climate_change_sims_' + cc_case + '.csv').set_index('SimNo')
 
 for sim in sims.itertuples():
-    if sim.Index>=0:
-        #pm.execute_notebook('d_ewr_setup.ipynb','-',parameters = dict(input_file='CC/ngonye_daily_' + str(cc_case) + '_' + str(sim.Index)))
-        #pm.execute_notebook('e_power_energy.ipynb','-',parameters = dict(input_file='CC/ngonye_daily_' + str(cc_case) + '_' + str(sim.Index)))
-        pm.execute_notebook('h_cc_summaries.ipynb','-',parameters = dict(input_file='ngonye_daily_' + cc_case + '_' + str(sim.Index) + '_base_pe'))
+    if sim.Index>=51:
+        pm.execute_notebook('d_ewr_setup.ipynb','-',parameters = dict(input_file='CC/ngonye_daily_' + str(cc_case) + '_' + str(sim.Index)))
+        pm.execute_notebook('e_power_energy.ipynb','-',parameters = dict(input_file='CC/ngonye_daily_' + str(cc_case) + '_' + str(sim.Index)))
+        pm.execute_notebook('h2_cc_summaries.ipynb','-',parameters = dict(input_file='ngonye_daily_' + cc_case + '_' + str(sim.Index) + '_base_pe'))
         
